a83dc87bb2da0da93c5f896899227754aabc8525
DavidCh98 <47038868+DavidCh98@users.noreply.github.com>
2020-03-22 18:15:40 +0100

    Board and pieces created

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScript : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+
+    private Vector3 boardOffset = new Vector3(-7.5f, 0, -7.5f);
+    private Vector3 pieceOffset = new Vector3(0.5f, 1.15f, 0.5f);
+
+    private void Start()
+    {
+        GenerateBoard();
+    }
+    private void GenerateBoard()
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x+=2)
+            {
+                GeneratePiece((oddRow) ? x : x+1, y);
+            }
+        }
+
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        bool isPieceWhite = (y > 3) ? false : true;
+        GameObject go = Instantiate((isPieceWhite)?whitePiecePrefab:blackPiecePrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x * 2) + (Vector3.forward * y * 2) + boardOffset + pieceOffset;
+    }
+
+}
